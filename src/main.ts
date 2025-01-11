8f5676f1dcb3606d639c1fd27e941ef6808ce75e
Thanh Huy <thhuy28062001@gmail.com>
2025-01-11 14:43:30 +0700

    Fix: deploy

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import 'tsconfig-paths/register';
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
